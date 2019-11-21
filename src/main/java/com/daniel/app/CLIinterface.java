ea81dfa7ed9af94d614377d914262c80849d0c3c
friikjones <d.mullerjones@gmail.com>
2019-11-21 12:44:00 -0800

    added Reader class

--- src/main/java/com/daniel/app/CLIinterface.java
+++ src/main/java/com/daniel/app/CLIinterface.java
@@ -1,5 +1,6 @@
 package com.daniel.app;
 
+import com.parser.JsonReader;
 import com.requester.app.*;;
 
 public class CLIinterface {
@@ -7,20 +8,24 @@ public class CLIinterface {
 	String receivedJson;
 	JsonSaver jsonSaver = new JsonSaver();
 	JsonRequester jsonRequester = new JsonRequester();
-	
+	JsonReader jsonReader = new JsonReader();
 	
 	public static void main(String[] args) {
 		CLIinterface cli = new CLIinterface();
-		cli.download("http://api.open-notify.org/iss-now.json", "issJson");
-		cli.download("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0", "insightJson");
-		
+		cli.downloadJson("http://api.open-notify.org/iss-now.json", "issJson");
+		cli.downloadJson("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0", "insightJson");
+		cli.parseJson("issJson");
 	}
 	
-	public void download(String url, String file) {
+	public void downloadJson(String url, String file) {
 		jsonRequester.makeRequest(url);
 		receivedJson = jsonRequester.getOutputRequestString();
-		jsonSaver = new JsonSaver();
 		jsonSaver.createOutput(file);
 		jsonSaver.writeToFile(receivedJson);
+	}
+	public void parseJson(String file) {
+		jsonReader.readFromFile(file);
+		receivedJson = jsonReader.getReaderString();
+		System.out.println(receivedJson);
 	}
 }
