f5a03423c3f53130d9bfcdf8bcd888f03e2b7fab
Avinoam Ruach <ruachavinoam@gmail.com>
2019-11-21 16:53:29 -0800

    adding the test files to check the json parser

--- src/CLIinterface.java
+++ src/CLIinterface.java
@@ -1,6 +1,15 @@
+import java.io.File;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CLIinterface {
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws JSONException {
+			
+		File aFile = new File("C:\\Users\\ruach\\Desktop\\Programming\\API-Project\\jsonFile.json");
+				JSONObject jsonObject = new JSONObject(aFile);
+				
+				jsonObject.get("Date");
+				
 	}
 }
