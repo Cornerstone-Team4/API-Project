04a05909f9b58c3005db5775c1b6b8dd865000d4
friikjones <d.mullerjones@gmail.com>
2019-11-26 12:27:00 -0800

    updates setters and getters

--- src/neows/Neo.java
+++ src/neows/Neo.java
@@ -5,12 +5,31 @@ public class Neo {
 	private String startDate; 
 	private String endDate;
 	private boolean hazardous; 
-	private int mnDiameter; //meters
-	private int mxDiameter; //meters
-	private int aproxVolume; //meters^3
-	private int estimatedMass; //kilos
+	private int mnDiameter; //kilometer
+	private int mxDiameter; //kilometer
+	private int aproxVolume; //kilometers^3
+	private int estimatedMass; //tons
+	private int missDistance; //kilometers
 	
-	
+	//Getters and Setters
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public boolean isHazardous() {
+		return hazardous;
+	}
+	public void setHazardous(boolean hazardous) {
+		this.hazardous = hazardous;
+	}
 	public int getMnDiameter() {
 		return mnDiameter;
 	}
@@ -23,23 +42,22 @@ public class Neo {
 	public void setMxDiameter(int mxDiameter) {
 		this.mxDiameter = mxDiameter;
 	}
-	public String getStartDate() {
-		return startDate;
+	public int getMissDistance() {
+		return missDistance;
 	}
-	public String getEndDate() {
-		return endDate;
+	public void setMissDistance(int missDistance) {
+		this.missDistance = missDistance;
 	}
-	public boolean getHazardous() {
-		return hazardous;
+	public int getEstimatedMass() {
+		estimatedMass = (int) (2*Math.pow(10, 9));
+		estimatedMass = estimatedMass * getAproxVolume();
+		return estimatedMass;
 	}
 	public int getAproxVolume() {
 		aproxVolume = (int) ((4/3)*Math.PI*mnDiameter*mnDiameter*mxDiameter);
 		return aproxVolume;
 	}
-	public int getEstimatedMass() {
-		estimatedMass = (int) ((4/3)*Math.PI*mnDiameter*mnDiameter*mxDiameter)*1000;
-		return estimatedMass;
-	}
+	
 	
 	
 	
